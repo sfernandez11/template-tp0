3c07fee80e8d02c78f9fcacd76508b79f0d98766
Santiago <fernandezsantid@gmail.com>
2016-03-21 13:41:37 -0300

    Agregados mas test, falta terminar de corregir los errores del checkstyle

--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.assertTrue;
 
 public class RegExGeneratorTest {
 
-    private int maxLength = 7;
+    private int maxLength = 20;
 
     private boolean validate(String regEx, int numberOfResults) {
         RegExGenerator generator = new RegExGenerator(this.maxLength);
@@ -37,7 +37,6 @@ public class RegExGeneratorTest {
     public void testAnyCharacter() {
         assertTrue(validate(".", 1));
     }
-
 
     @Test
     public void testMultipleCharacters() {
@@ -69,5 +68,134 @@ public class RegExGeneratorTest {
         assertTrue(validate("[abc]+", 1));
     }
 
-    // TODO: Add more tests!!!
+
+    @Test
+    public void testZeroOrMoreCharacter() {
+        assertTrue(validate("z*", 10));
+    }
+
+    @Test
+    public void testNegativeOrError() {
+        assertFalse(validate("jkl++", 10));
+        assertFalse(validate("[]", 15));
+        assertFalse(validate("[[e]", 10));
+        assertFalse(validate("[]]", 10));
+    }
+
+    @Test
+    public void testDotWithQuantifiers() {
+        assertTrue(validate(".*", 10));
+        assertTrue(validate(".+", 10));
+        assertTrue(validate(".?", 10));
+    }
+
+    @Test
+    public void testALotOfSets() {
+        assertTrue(validate("[aeqdc]q[wdca]+[hjk]*[rwxc]?..*", 10));
+    }
+
+    @Test
+    //No puede haber caracteres reservados dentro de un Set
+    public void testEscapedLiteralsOnSet() {
+        assertFalse(validate("[scsd[]dwd]*", 5));
+        assertFalse(validate("[a*dwdq]", 10));
+        assertFalse(validate("[a3?]", 10));
+        assertFalse(validate("[avvcs+]", 10));
+        assertFalse(validate("[qw.c??]", 20));
+    }
+
+    @Test
+    public void testEscapedLiteralWithQuantifiers() {
+        assertTrue(validate("\\.*", 10));
+        assertTrue(validate("\\+*", 10));
+        assertTrue(validate("\\?*", 10));
+        assertTrue(validate("\\**", 10));
+        assertTrue(validate("\\+?", 10));
+        assertTrue(validate("\\*+", 10));
+    }
+
+    @Test
+    public void testExhaustive() {
+        assertTrue(validate("ab*.+\\h\\+[cde]?.[fg]", 1));
+    }
+
+    @Test
+    public void testVeryExhaustive() {
+        assertTrue(validate("wqv\\..+d*w?[zxy]+p", 50));
+    }
+
+    @Test
+    public void testAnyCharacterWithAsterisk() {
+        assertTrue(validate(".*", 1));
+    }
+
+    @Test
+    public void testLiteralWithAsterisk() {
+        assertTrue(validate("&*", 1));
+    }
+
+    @Test
+    public void testAnyCharacterWithPlus() {
+        assertTrue(validate(".*", 1));
+    }
+
+    @Test
+    public void testLiteralWithPlus() {
+        assertTrue(validate("´+", 1));
+    }
+
+    @Test
+    public void testAnyCharacterWithQuestionMark() {
+        assertTrue(validate(".?", 1));
+    }
+
+    @Test
+    public void testMultipleAnyCharacters() {
+        assertTrue(validate(".*.*.*", 1));
+    }
+
+    @Test
+    public void testaaaMultipleAnyaCharacters() {
+        assertTrue(validate("2.123.*", 1));
+    }
+
+    @Test
+    public void testBackslashedDotWithLiteral() {
+        assertTrue(validate("\\..72.*", 1));
+    }
+
+    @Test
+    public void testBackslashedDot() {
+        assertTrue(validate("\\.", 1));
+    }
+
+    @Test
+    public void testBackslashedLiteral() {
+        assertTrue(validate("\\7", 1));
+    }
+
+    @Test
+    public void testNumber() {
+        assertTrue(validate("23.2212", 1));
+    }
+
+    @Test
+    public void testAlpha() {
+        assertTrue(validate("sdsd+.6.231.3.*", 1));
+    }
+
+    @Test
+    public void testWithBackSlashAndAlpha() {
+        assertTrue(validate("\\*zxe?.2.3.1", 1));
+    }
+
+    @Test
+    public void testNormalSetAndLiteral() {
+        assertTrue(validate("[asvg]llld", 1));
+    }
+
+    @Test
+    public void testNormalSetAndLiteralWithQuantifier() {
+        assertTrue(validate("[asvg]llld*", 1));
+    }
 }
